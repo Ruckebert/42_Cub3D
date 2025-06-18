3561be6641a9503deff4c14a47572a73a3a7b667
Albert Aiyakup Ruckenbauer <aruckenb@c2r3p10.42vienna.com>
2025-06-18 13:58:51 +0200

    Everything seems fine now?

--- Parsing/utils2.c
+++ Parsing/utils2.c
@@ -6,24 +6,32 @@
 /*   By: aruckenb <aruckenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 10:17:37 by aruckenb          #+#    #+#             */
-/*   Updated: 2025/06/11 11:37:57 by aruckenb         ###   ########.fr       */
+/*   Updated: 2025/06/18 13:57:48 by aruckenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
-int	rgbloop(char *line, int i, int *type)
+int	rgbloop(char *line, long long i, int *type)
 {
+	int	count;
+
+	count = 0;
 	*type = ft_atoi1(&line[i]);
 	while (line[i] && ft_isdigit(line[i]))
+	{
 		i++;
+		count++;
+		if (count > 3)
+			return (-1);
+	}
 	if (line[i] != ',')
 		return (-1);
 	i++;
 	return (i);
 }
 
-int	rgbfirst(char *line, int i)
+int	rgbfirst(char *line, long long i)
 {
 	while (line[i] && !ft_isdigit(line[i]))
 	{
