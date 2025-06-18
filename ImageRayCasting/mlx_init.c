3e245e7552330e8b9ddc000b7db2e79edbc96b7e
Martin Arsenijevic <martin.arsenijevic@gmail.com>
2025-06-18 13:35:16 +0200

    4

--- ImageRayCasting/mlx_init.c
+++ ImageRayCasting/mlx_init.c
@@ -6,7 +6,7 @@
 /*   By: marsenij <marsenij@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 13:52:08 by marsenij          #+#    #+#             */
-/*   Updated: 2025/06/18 13:02:48 by marsenij         ###   ########.fr       */
+/*   Updated: 2025/06/18 13:17:53 by marsenij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,21 +72,6 @@ static int	load_textures(t_data *core, t_game *game)
 	return (0);
 }
 
-static void	load_texture_data(t_game *game)
-{
-	game->tex_north.data = mlx_get_data_addr(game->tex_north.img,
-			&game->tex_north.bpp, &game->tex_north.line_len,
-			&game->tex_north.endian);
-	game->tex_south.data = mlx_get_data_addr(game->tex_south.img,
-			&game->tex_south.bpp, &game->tex_south.line_len,
-			&game->tex_south.endian);
-	game->tex_east.data = mlx_get_data_addr(game->tex_east.img,
-			&game->tex_east.bpp, &game->tex_east.line_len,
-			&game->tex_east.endian);
-	game->tex_west.data = mlx_get_data_addr(game->tex_west.img,
-			&game->tex_west.bpp, &game->tex_west.line_len,
-			&game->tex_west.endian);
-}
 
 int	init(t_data *core, t_game *game)
 {
