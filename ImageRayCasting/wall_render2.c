0d640f0fffc1ece057007b83849f21c659d798b9
makiars <martin.arsenijevic@gmail.com>
2025-06-18 11:41:00 +0200

    1

--- ImageRayCasting/wall_render2.c
+++ ImageRayCasting/wall_render2.c
@@ -12,36 +12,7 @@
 
 #include "../header.h"
 
-static int	calculate_slice_height(t_game *game, double corr_dist)
-{
-	double	proj_dist;
-	int		slice_h;
-	double	fov;
-
-	fov = M_PI / 3;
-	proj_dist = (game->win_x / 2.0) / tan(fov / 2.0);
-	slice_h = (int)(proj_dist / corr_dist);
-	return (slice_h);
-}
-
-static void	set_bounds(int *start, int *end, int slice_h, t_game *game)
-{
-	*start = (game->win_y / 2) - (slice_h / 2);
-	*end = *start + slice_h;
-	if (*start < 0)
-		*start = 0;
-	if (*end >= game->win_y)
-		*end = game->win_y - 1;
-}
-
-void	compute_wall_dimensions(t_game *game, double corr_dist,
-		int *start, int *end)
-{
-	int	slice_h;
-
-	slice_h = calculate_slice_height(game, corr_dist);
-	set_bounds(start, end, slice_h, game);
-}
+#include "../header.h"
 
 t_texture	*select_wall_texture(t_game *game, t_ray *ray, t_dda *dda)
 {
@@ -61,7 +32,6 @@ t_texture	*select_wall_texture(t_game *game, t_ray *ray, t_dda *dda)
 double	get_wall_x(t_game *game, t_ray *ray, t_dda *dda)
 {
 	double	wall_x;
-
 	if (dda->side == 0)
 		wall_x = game->py + dda->perp_dist * ray->dir_y;
 	else
--- ImageRayCasting/wall_render3.c
+++ ImageRayCasting/wall_render3.c
@@ -1,22 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   wall_render3.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aruckenb <aruckenb@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/29 14:10:00 by marsenij          #+#    #+#             */
-/*   Updated: 2025/06/11 11:37:34 by aruckenb         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../header.h"
+
+static int	calculate_slice_height(t_game *game, double corr_dist)
+{
+	double	proj_dist;
+	int		slice_h;
+	double	fov;
+	fov = M_PI / 3;
+	proj_dist = (game->win_x / 2.0) / tan(fov / 2.0);
+	if (corr_dist < 0.0001)
+		corr_dist = 0.0001;
+	slice_h = (int)(proj_dist / corr_dist);
+	return (slice_h);
+}
 
 int	compute_texture_x(t_game *game, t_ray *ray, t_dda *dda, t_texture *tex)
 {
 	double	wall_x;
 	int		tex_x;
-
 	wall_x = get_wall_x(game, ray, dda);
 	tex_x = (int)(wall_x * tex->width);
 	if ((dda->side == 0 && ray->dir_x < 0.0)
@@ -28,30 +28,23 @@ int	compute_texture_x(t_game *game, t_ray *ray, t_dda *dda, t_texture *tex)
 int	get_texture_pixel(t_texture *tex, int x, int y)
 {
 	int	offset;
-
 	offset = y * tex->line_len + x * (tex->bpp / 8);
 	return (*(int *)(tex->data + offset));
-}
-
-static void	set_wall_params_basic(t_wall_params *p, int start, int end)
-{
-	int	slice_h;
-
-	slice_h = end - start;
-	p->start = start;
-	p->end = end;
-	p->original_start = start;
-	p->wall_height = slice_h;
 }
 
 t_wall_params	compute_wall_params(t_game *game, t_raycast_result *res)
 {
 	t_wall_params	p;
-	int				start;
-	int				end;
-
-	compute_wall_dimensions(game, res->corrected_dist, &start, &end);
-	set_wall_params_basic(&p, start, end);
+	int				slice_h;
+	slice_h = calculate_slice_height(game, res->corrected_dist);
+	p.wall_height = slice_h;
+	p.original_start = (game->win_y / 2) - (slice_h / 2);
+	p.start = p.original_start;
+	p.end = p.original_start + slice_h;
+	if (p.start < 0)
+		p.start = 0;
+	if (p.end >= game->win_y)
+		p.end = game->win_y - 1;
 	p.tex = select_wall_texture(game, &res->ray, &res->dda);
 	p.tex_x = compute_texture_x(game, &res->ray, &res->dda, p.tex);
 	return (p);
@@ -60,7 +53,6 @@ t_wall_params	compute_wall_params(t_game *game, t_raycast_result *res)
 void	draw_ceiling(t_game *game, int col, int start)
 {
 	int	y;
-
 	y = 0;
 	while (y < start)
 	{
